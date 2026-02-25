ea1b9126b9c0a0eeb7350d13b60e79f68dad7b0e
suspen <190865826+shusoyo@users.noreply.github.com>
2026-02-25 23:19:30 +0800

    ~. fix parser

--- lib/parser.ml
+++ lib/parser.ml
@@ -18,12 +18,15 @@ let char c = lex (char c)
 let parens p = char '(' *> p <* char ')'
 let braces p = char '{' *> p <* char '}'
 let arrow = symbol "->" <|> symbol "→"
+let lambda = symbol "\\" <|> symbol "λ"
 let ( let* ) = Oscar.bind
-let keyword s = List.mem s [ "let"; "Type"; "λ"; "U" ]
+let is_keyword s = List.mem s [ "let"; "Type"; "λ"; "U" ]
 
 let parse_ident : name parser =
   let* x = take_while1 (fun c -> P.is_alphanum c) in
-  if keyword x then empty else lex (return x)
+  if is_keyword x then empty else lex (return x)
+
+let parse_binder = parse_ident <|> symbol "_"
 
 let parse_keywrod s =
   let* kw = string s in
@@ -33,37 +36,37 @@ let parse_keywrod s =
 let digits = lex (int_of_string <$> take_while1 P.is_digit)
 
 let term_parser =
-  let mk_let n x y z = Let (n, x, y, z) in
   let mk_app x xs = List.fold_left (fun acc (i, t) -> App (acc, t, i)) x xs in
   let mk_var n = Var n in
   let mk_universe _ = Universe in
   let mk_hole _ = Hole in
-  let lambda = symbol "\\" <|> symbol "λ" in
   fix (fun raw ->
+    (* U (Universe), x (identifier), t (raw term) *)
     let parse_atom =
       mk_var <$> parse_ident
-      <|> (mk_universe <$> symbol "U")
-      <|> (mk_hole <$> symbol "_")
+      <|> (mk_universe <$> char 'U')
+      <|> (mk_hole <$> char '_')
       <|> parens raw
     in
 
+    (* {x = t}, {y}, z *)
     let parser_args =
       braces ((fun n t -> (Left n, t)) <$> parse_ident <* char '=' <*> raw)
-      <|> ((fun t -> (Right Explicit, t)) <$> parens raw)
+      <|> ((fun t -> (Right Explicit, t)) <$> parse_atom)
       <|> ((fun t -> (Right Implicit, t)) <$> braces raw)
     in
 
+    (* a {x = t} {y} z  *)
     let parse_spine = mk_app <$> parse_atom <*> many parser_args in
-
-    let parse_binder = parse_ident <|> symbol "_" in
 
     let parse_lam_binder =
       (fun x -> (x, Right Explicit))
-      <$> parse_ident
+      <$> parse_binder
       <|> ((fun x -> (x, Right Implicit)) <$> braces parse_binder)
       <|> braces ((fun n t -> (t, Left n)) <$> parse_ident <* char '=' <*> parse_binder)
     in
 
+    (* \{x = a} {y} z . t *)
     let parse_lam =
       (fun names body -> List.fold_right (fun (n, i) b -> Lam (n, i, b)) names body)
       <$> lambda *> many1 parse_lam_binder
@@ -93,9 +96,10 @@ let term_parser =
     in
 
     let parse_let =
-      (fun n a b d -> mk_let n a b d)
+      (fun n x y z -> Let (n, Option.value x ~default:Hole, y, z))
       <$> parse_keywrod "let" *> parse_binder
-      <* char ':' <*> raw <* symbol "=" <*> raw <* symbol ";" <*> raw
+      <*> optional (char ':' *> raw)
+      <* symbol "=" <*> raw <* symbol ";" <*> raw
     in
 
     parse_lam <|> parse_let <|> parse_pi <|> parse_fun_or_spine)
